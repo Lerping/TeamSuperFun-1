ddada692f3dd2ccab632b5e3cf7120d2ace4b0c5
Ethan Werner <ethanwerner99@gmail.com>
2021-11-01 15:48:19 -0700

    Make controller for /movies endpoint

---
+++ src/main/java/com/tsf/controller/SearchController.java
@@ -0,0 +1,13 @@
+package com.tsf.controller;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tsf.core.*;
+
+@RestController
+@RequestMapping("/search")
+public class SearchController {
+
+}
--- src/main/java/com/tsf/controller/TestController.java
+++ src/main/java/com/tsf/controller/TestController.java
@@ -3,9 +3,6 @@ package com.tsf.controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.JsonNode;
 
 import com.tsf.core.*;
 
