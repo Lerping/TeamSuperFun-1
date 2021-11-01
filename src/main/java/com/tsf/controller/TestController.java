d0062ba5640b157d6c882c3ed86adea99476a5bf
Ethan Werner <ethanwerner99@gmail.com>
2021-11-01 13:58:56 -0700

    Add com.tsf.controller package

---
+++ src/main/java/com/tsf/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.tsf.controller;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import com.tsf.core.*;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+}
